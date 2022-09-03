1545e694227d9eee76487fa77877b749754ae315
WilliamJSS <williamsena088@gmail.com>
2022-09-03 11:04:18 -0300

    Pratica 1

--- triangulo.cpp
+++ triangulo.cpp
@@ -1,47 +1,72 @@
 #include <stdlib.h>
+#include <iostream>
+#include <locale.h>
 #include <GL/glut.h>
 
-void Desenha(void) // Função callback de redesenho da janela de visualização
+// Função callback de redesenho da janela de visualização
+void Desenha(void)
 {
-// Limpa a janela de visualização com a cor branca
-glClearColor(1,1,1,0); glClear(GL_COLOR_BUFFER_BIT);
-glColor3f(1,0,0); // Define a cor de desenho: vermelho
-// Desenha um triângulo no centro da janela
+	// Alteracao 1 - cor do fundo amarelo
+	// Limpa a janela de visualização com a cor amarela
+	glClearColor(1, 1, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-glBegin(GL_TRIANGLES);
-glVertex3f(-0.5,-0.5,0);
-glVertex3f( 0.0, 0.5,0);
-glVertex3f( 0.5,-0.5,0);
-glEnd();
-               
-glFlush(); //Executa os comandos OpenGL
+	// Alteracao 2 - gradiente de cor
+	// Desenha um triângulo no centro da janela
+	glColor3f(0, 1, 0);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-1.0, -0.5, 0);
+	glColor3f(1, 0, 0);
+	glVertex3f(-0.5, 0.5, 0);
+	glVertex3f(0.0, -0.5, 0);
+	glEnd();
+
+	// Alteracao 4 - desenhar um quadrado
+	glColor3f(0, 0, 1);
+	glBegin(GL_QUADS);
+	glVertex3f(0.0, -0.5, 0);
+	glVertex3f(0.0, 0.5, 0);
+	glVertex3f(1.0, 0.5, 0);
+	glVertex3f(1.0, -0.5, 0);
+	glEnd();
+
+	glFlush(); // Executa os comandos OpenGL
 }
 
 // Função callback chamada para gerenciar eventos de teclas
-void Teclado (unsigned char key, int x, int y) {
-if (key == 27)
-exit(0);
-}
-// Função responsável por inicializar parâmetros e variáveis
-void Inicializa(void) {
-// Define a janela de visualização 2D
-glMatrixMode(GL_PROJECTION);
-gluOrtho2D(-1.0,1.0,-1.0,1.0);
-glMatrixMode(GL_MODELVIEW);
+void Teclado(unsigned char key, int x, int y)
+{
+	if (key == 27)
+		exit(0);
 }
 
-int main (int argc, char **argv){
+// Função responsável por inicializar parâmetros e variáveis
+void Inicializa(void)
+{
+	// Define a janela de visualização 2D
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);						 // Programa Principal
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define do modo de operação da GLUT
+	glutInitWindowSize(400, 400);				 // tamanho em pixels da janela
 	
-	glutInit(&argc, argv);// Programa Principal
-// Define do modo de operação da GLUT
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-glutInitWindowSize(400,400); // tamanho em pixels da janela
-// Cria a janela passando como argumento o título da mesma
-glutCreateWindow("Primeiro Programa");
-glutDisplayFunc(Desenha); // Registra função de redesenho
-glutKeyboardFunc (Teclado); // Registra função de teclado
-Inicializa(); // Chama a função de inicializações
-// Inicia o processamento e aguarda interações do usuário
-glutMainLoop();
-return 0;
+	// Alteracao 3 - Titulo da Janela
+	glutCreateWindow("Pratica Computacao Grafica"); // Cria a janela
+
+	glutDisplayFunc(Desenha);  // Registra função de redesenho
+	glutKeyboardFunc(Teclado); // Registra função de teclado
+	Inicializa();			   // Chama a função de inicializações
+
+	// Alteracao 5 - transformacoes geometricas
+	glTranslatef(0, -0.5, 0);
+	glScalef(0.5, 0.5, 0);
+
+	glutMainLoop();			   // Inicia o processamento e aguarda interações do usuário
+
+	return 0;
 }

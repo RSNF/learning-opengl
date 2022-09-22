935cfcaa4d93e32c615284456339f2864abc0881
WilliamJSS <williamsena088@gmail.com>
2022-09-21 23:58:41 -0300

    atualizado metodo de armazenamento

--- desafios/desafio1.cpp
+++ desafios/desafio1.cpp
@@ -1,13 +1,6 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 
-struct Color
-{
-    GLfloat r;
-    GLfloat g;
-    GLfloat b;
-};
-
 struct Point
 {
     GLfloat x;
@@ -16,71 +9,97 @@ struct Point
 
 struct Object
 {
-    Color color;
-    Point point;
+    GLfloat cor[3];
     GLfloat width;
     GLfloat height;
+    GLfloat x, y;
 };
 
+struct Foguete
+{
+    Object bico;
+    Object corpo;
+    Object asaEsq;
+    Object asaDir;
+};
+
+// Variaveis globais
 int hp, qntObstaculos;
-Point startPoint, currentPoint, hpStartPoint;
+Point moveFoguete;
+Foguete foguete, fogueteVidas;
 Object obstaculos[10];
 
+// Prototipos das funcoes
+void Bico(Object obj);
+void Corpo(Object obj);
+void AsaEsquerda(Object obj);
+void AsaDireita(Object obj);
+void DesenhaFoguete(Foguete foguete);
+void DesenhaQuadro();
+void DesenhaObstaculo(Object obj);
+void Desenha();
+void Teclado(unsigned char key, int x, int y);
+void Movimentacao(int key, int x, int y);
+Object InicializaObjeto(
+    GLfloat x, GLfloat y, GLfloat h, GLfloat w,
+    GLfloat r = 1, GLfloat g = 1, GLfloat b = 0);
+void Inicializa(void);
+
 // Desenha o bico do foguete
-void Bico(GLfloat x, GLfloat y)
+void Bico(Object obj)
 {
     glBegin(GL_TRIANGLES);
     glColor3f(0, 1, 0);
-    glVertex2f(x, y + 10);
-    glVertex2f(x + 2, y + 14);
-    glVertex2f(x + 4, y + 10);
+    glVertex2f(obj.x, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y);
+    glVertex2f(obj.x + obj.width / 2, obj.y + obj.height);
     glEnd();
 }
 
 // Desenha o corpo do foguete
-void Corpo(GLfloat x, GLfloat y)
+void Corpo(Object obj)
 {
     glBegin(GL_QUADS);
     glColor3f(0, 0, 1);
-    glVertex2f(x, y);
-    glVertex2f(x + 4, y);
-    glVertex2f(x + 4, y + 10);
-    glVertex2f(x, y + 10);
+    glVertex2f(obj.x, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y + obj.height);
+    glVertex2f(obj.x, obj.y + obj.height);
     glEnd();
 }
 
 // Desenha a asa esquerda do foguete
-void AsaEsquerda(GLfloat x, GLfloat y)
+void AsaEsquerda(Object obj)
 {
     glBegin(GL_TRIANGLES);
     glColor3f(1, 0, 0);
-    glVertex2f(x - 3, y);
-    glVertex2f(x, y);
-    glVertex2f(x, y + 4);
+    glVertex2f(obj.x, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y + obj.height);
     glEnd();
 }
 
 // Desenha a asa direita do foguete
-void AsaDireita(GLfloat x, GLfloat y)
+void AsaDireita(Object obj)
 {
     glBegin(GL_TRIANGLES);
     glColor3f(1, 0, 0);
-    glVertex2f(x + 4, y);
-    glVertex2f(x + 7, y);
-    glVertex2f(x + 4, y + 4);
+    glVertex2f(obj.x, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y);
+    glVertex2f(obj.x, obj.y + obj.height);
     glEnd();
 }
 
 // Funcao para desenhar um foguete
-void DesenhaFoguete(GLfloat x, GLfloat y)
+void DesenhaFoguete(Foguete foguete)
 {
-    Bico(x, y);
-    Corpo(x, y);
-    AsaEsquerda(x, y);
-    AsaDireita(x, y);
+    Bico(foguete.bico);
+    Corpo(foguete.corpo);
+    AsaEsquerda(foguete.asaEsq);
+    AsaDireita(foguete.asaDir);
 }
 
-// Desenha o quadro onde o foguete pode se movimentar
+// Desenha o quadro correspondente a area onde o foguete pode se movimentar
 void DesenhaQuadro()
 {
     glLineWidth(7.0);
@@ -98,21 +117,21 @@ void DesenhaObstaculo(Object obj)
 {
     // Desenha o obstaculo
     glBegin(GL_QUADS);
-    glColor3f(obj.color.r, obj.color.g, obj.color.b);
-    glVertex2f(obj.point.x, obj.point.y);
-    glVertex2f(obj.point.x + obj.width, obj.point.y);
-    glVertex2f(obj.point.x + obj.width, obj.point.y + obj.height);
-    glVertex2f(obj.point.x, obj.point.y + obj.height);
+    glColor3f(obj.cor[0], obj.cor[1], obj.cor[2]);
+    glVertex2f(obj.x, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y + obj.height);
+    glVertex2f(obj.x, obj.y + obj.height);
     glEnd();
 
     // Desenha a linha da borda do obstaculo
     glLineWidth(2.0);
     glBegin(GL_LINE_LOOP);
     glColor3f(1, 0, 0);
-    glVertex2f(obj.point.x, obj.point.y);
-    glVertex2f(obj.point.x + obj.width, obj.point.y);
-    glVertex2f(obj.point.x + obj.width, obj.point.y + obj.height);
-    glVertex2f(obj.point.x, obj.point.y + obj.height);
+    glVertex2f(obj.x, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y);
+    glVertex2f(obj.x + obj.width, obj.y + obj.height);
+    glVertex2f(obj.x, obj.y + obj.height);
     glEnd();
 }
 
@@ -127,12 +146,19 @@ void Desenha()
     DesenhaQuadro();
 
     // Desenha o foguete do jogador
-    DesenhaFoguete(currentPoint.x, currentPoint.y);
+    glPushMatrix();
+    glTranslatef(moveFoguete.x, moveFoguete.y, 0);
+    DesenhaFoguete(foguete);
+    glPopMatrix();
 
     // Desenha os foguetes das vidas
-    DesenhaFoguete(hpStartPoint.x, hpStartPoint.y);
-    DesenhaFoguete(hpStartPoint.x - 13, hpStartPoint.y);
-    DesenhaFoguete(hpStartPoint.x - 26, hpStartPoint.y);
+    glPushMatrix();
+    for (int i = 0; i < hp; i++)
+    {
+        glTranslatef(-13, 0, 0);
+        DesenhaFoguete(fogueteVidas);
+    }
+    glPopMatrix();
 
     // Desenha os obstaculos
     for (int i = 0; i < qntObstaculos; i++)
@@ -152,45 +178,42 @@ void Teclado(unsigned char key, int x, int y)
 // Funcao callback chamada para movimentar o foguete
 void Movimentacao(int key, int x, int y)
 {
-    if (key == GLUT_KEY_UP && currentPoint.y < 60)
-    {
-        currentPoint.y += 5;
-    }
-    if (key == GLUT_KEY_DOWN && currentPoint.y > startPoint.y)
-    {
-        currentPoint.y -= 5;
-    }
-    if (key == GLUT_KEY_LEFT && currentPoint.x > -90)
-    {
-        currentPoint.x -= 5;
-    }
-    if (key == GLUT_KEY_RIGHT && currentPoint.x < 85)
-    {
-        currentPoint.x += 5;
-    }
+    if (key == GLUT_KEY_UP && moveFoguete.y < 150)
+        moveFoguete.y += 5;
+
+    if (key == GLUT_KEY_DOWN && moveFoguete.y > 0)
+        moveFoguete.y -= 5;
+
+    if (key == GLUT_KEY_LEFT && moveFoguete.x > -90)
+        moveFoguete.x -= 5;
+
+    if (key == GLUT_KEY_RIGHT && moveFoguete.x < 85)
+        moveFoguete.x += 5;
+
     glutPostRedisplay();
 }
 
-// Funcao responsavel por definir os obstaculos
-Object InicializaObstaculo(GLfloat x, GLfloat y, GLfloat h, GLfloat w,
-                           GLfloat r, GLfloat g, GLfloat b)
+// Funcao responsavel por definir os objetos
+Object InicializaObjeto(
+    GLfloat x, GLfloat y, GLfloat h, GLfloat w,
+    GLfloat r, GLfloat g, GLfloat b)
 {
-    Object obs;
+    Object obj;
 
-    // Posicao do obstaculo
-    obs.point.x = x;
-    obs.point.y = y;
+    // Posicao de referencia
+    obj.x = x;
+    obj.y = y;
 
     // Dimensoes
-    obs.height = h;
-    obs.width = w;
+    obj.height = h;
+    obj.width = w;
 
     // Cor
-    obs.color.r = r;
-    obs.color.g = g;
-    obs.color.b = b;
+    obj.cor[0] = r;
+    obj.cor[1] = g;
+    obj.cor[2] = b;
 
-    return obs;
+    return obj;
 }
 
 // Funcao responsavel por inicializar parametros e variaveis
@@ -199,25 +222,29 @@ void Inicializa(void)
     // Quantidade de vidas/tentativas do jogador
     hp = 3;
 
-    // Posicao inicial do foguete
-    startPoint.x = 0;
-    startPoint.y = -90;
+    // Valor da translacao inicial do foguete
+    moveFoguete.x = 0;
+    moveFoguete.y = 0;
 
-    // Posicao inicial do foguete das vidas
-    hpStartPoint.x = 90;
-    hpStartPoint.y = 83;
+    // Dimensoes do foguete principal
+    foguete.bico = InicializaObjeto(0, -80, 3.99, 3.99);
+    foguete.corpo = InicializaObjeto(0, -90, 9.99, 3.99);
+    foguete.asaEsq = InicializaObjeto(-3, -90, 3.99, 2.99);
+    foguete.asaDir = InicializaObjeto(4, -90, 3.99, 2.99);
 
-    // Posicao atual do foguete
-    currentPoint.x = startPoint.x;
-    currentPoint.y = startPoint.y;
+    // Dimensoes do foguete das vidas
+    fogueteVidas.bico = InicializaObjeto(103, 93, 4, 4);
+    fogueteVidas.corpo = InicializaObjeto(103, 83, 10, 4);
+    fogueteVidas.asaEsq = InicializaObjeto(100, 83, 4, 3);
+    fogueteVidas.asaDir = InicializaObjeto(107, 83, 4, 3);
 
-    // Obstaculos
-    obstaculos[0] = InicializaObstaculo(10, 10, 10, 10, 1, 1, 0);
-    obstaculos[1] = InicializaObstaculo(30, 40, 10, 10, 1, 1, 0);
-    obstaculos[2] = InicializaObstaculo(-50, -10, 10, 10, 1, 1, 0);
-    obstaculos[3] = InicializaObstaculo(-20, 10, 10, 10, 1, 1, 0);
-    obstaculos[4] = InicializaObstaculo(-10, -60, 10, 30, 1, 1, 0);
-    obstaculos[5] = InicializaObstaculo(-40, -70, 40, 10, 1, 1, 0);
+    // Dimensoes dos obstaculos
+    obstaculos[0] = InicializaObjeto(10, 10, 10, 10);
+    obstaculos[1] = InicializaObjeto(30, 40, 10, 10);
+    obstaculos[2] = InicializaObjeto(-50, -10, 10, 10);
+    obstaculos[3] = InicializaObjeto(-20, 10, 10, 10);
+    obstaculos[4] = InicializaObjeto(-10, -60, 10, 30);
+    obstaculos[5] = InicializaObjeto(-40, -70, 40, 10);
 
     qntObstaculos = 6;
 
